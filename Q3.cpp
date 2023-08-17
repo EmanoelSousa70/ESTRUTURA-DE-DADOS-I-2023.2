54b651c8fbe95991e8276aad6aa98a25b74914b5
EmanoelSousa70 <61412472+EmanoelSousa70@users.noreply.github.com>
2023-08-17 09:29:38 -0300

    Create Q3.cpp

---
+++ Q3.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+char f1[10];
+char f2[10];
+
+int main() {
+    printf("Digite a primeira string: ");
+    scanf("%s", f1);
+
+    printf("Digite a segunda string: ");
+    scanf("%s", f2);
+    
+    if (strcmp(f1, f2) == 0) {
+        printf("São iguais\n");
+    } else {
+        printf("São diferentes!!\n");
+    }
+    
+    return 0;
+}
+
+
