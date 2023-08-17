364f4457a0d9038f6fa7d2be6e073e94069b6d83
EmanoelSousa70 <61412472+EmanoelSousa70@users.noreply.github.com>
2023-08-17 09:32:28 -0300

    Update Create Q3.2.cpp

--- Create Q3.2.cpp
+++ Create Q3.2.cpp
@@ -1,1 +1,25 @@
+#include <stdio.h>
 
+char f1[10];
+char f2[10];
+
+int main() {
+    printf("Digite a primeira string: ");
+    scanf("%s", f1);
+
+    printf("Digite a segunda string: ");
+    scanf("%s", f2);
+    
+    int i = 0;
+    while (f1[i] != '\0' && f2[i] != '\0' && f1[i] == f2[i]) {
+        i++;
+    }
+
+    if (f1[i] == '\0' && f2[i] == '\0') {
+        printf("São iguais\n");
+    } else {
+        printf("São diferentes!!\n");
+    }
+    
+    return 0;
+}
